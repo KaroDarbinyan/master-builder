eac0fe9f3e7286c69754bb5898e9454e4c2da89d
Karo Darbinyan <darbinyan.karo@gmail.com>
2021-01-29 17:16:47 +0400

    initial commit

---
+++ src/main/java/ru/masterbuilder/MasterBuilderApplication.java
@@ -0,0 +1,13 @@
+package ru.masterbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/masterbuilder/MasterBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.masterbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
